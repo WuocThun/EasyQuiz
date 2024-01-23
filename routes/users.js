25d9a0188df77766d1942b1fd7e0609acc7d36e3
WuocThun <binngoc1963@gmail.com>
2024-01-24 00:40:41 +0700

    wuocThun

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const userRoutes = (app, fs) => {
   // variables
-  const dataPath = './data/users.json';
+  const dataPath = "./data/users.json";
 
   // READ
-  app.get('/user/userType/61403e8fa8524b0019c83a3e', (req, res) => {
-    fs.readFile(dataPath, 'utf8', (err, data) => {
+  app.get("/user/userType/61403e8fa8524b0019c83a3e", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
       if (err) {
         throw err;
       }
@@ -13,13 +13,13 @@ const userRoutes = (app, fs) => {
     });
   });
 
-//võ
-    // variables
-  const dataVo = './data/vo.json';
+  //võ
+  // variables
+  const dataVo = "./data/vo.json";
 
   // READ
-  app.get('/user/userType/6151be1450bbd90019387ea9', (req, res) => {
-    fs.readFile(dataVo, 'utf8', (err, data) => {
+  app.get("/user/userType/6151be1450bbd90019387ea9", (req, res) => {
+    fs.readFile(dataVo, "utf8", (err, data) => {
       if (err) {
         throw err;
       }
@@ -27,8 +27,19 @@ const userRoutes = (app, fs) => {
     });
   });
 
+  //võ
+  // variables
+  const dataThuan = "./data/thuan.json";
 
-  
+  // READ
+  app.get("/user/userType/thuan", (req, res) => {
+    fs.readFile(dataThuan, "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.send(JSON.parse(data));
+    });
+  });
 };
 
 module.exports = userRoutes;
